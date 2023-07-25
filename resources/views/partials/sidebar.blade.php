acf8a6abb361b9552470b4710a0d68d30ff62067
PonoNgatui <ponongatui8@gmail.com>
2023-07-25 22:52:28 +0700

    develop home view1

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <div class="sidebar-container">
+      <a class="sidebar-link" href="{{ route('home') }}">
+        <span class="material-icons sidebar-icon">home</span>
+        <p class="sidebar-text">Home</p>
+      </a>
+      
+    </div>
+  </div>
